ba3bd248d53ead7e818f9c4924e3c15e1a318f1b
andreevskaivana <ivana.andreevska25@gmail.com>
2023-09-06 23:27:30 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import PokemonList from "./components/PokemonList";
+import Pagination from "./components/Pagination";
+import axios from "axios";
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [prevPageUrl, setPrevPageUrl] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    let cancel;
+
+    axios
+      .get(currentPageUrl, {
+        cancelToken: new axios.CancelToken((c) => (cancel = c)),
+      })
+      .then((res) => {
+        setLoading(false);
+        setNextPageUrl(res.data.next);
+        setPrevPageUrl(res.data.previous);
+        setPokemon(res.data.results.map((p) => p.name));
+        setError(null); 
+      })
+      .catch((err) => {
+        if (axios.isCancel(err)) {
+          return;
+        }
+        setError("An error occurred while fetching data.");
+        setLoading(false);
+      });
+
+    return () => {
+      if (cancel) {
+        cancel();
+      }
+    };
+  }, [currentPageUrl]);
+
+  function goToNextPage() {
+    setCurrentPageUrl(nextPageUrl);
+  }
+
+  function goToPrevPage() {
+    setCurrentPageUrl(prevPageUrl);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Pokemon List</h1>
+      {loading ? (
+        "Loading..."
+      ) : error ? (
+        <div>Error: {error}</div>
+      ) : (
+        <>
+          <PokemonList pokemon={pokemon} />
+          <Pagination
+            goToNextPage={nextPageUrl ? goToNextPage : null}
+            goToPrevPage={prevPageUrl ? goToPrevPage : null}
+          />
+        </>
+      )}
     </div>
   );
 }
